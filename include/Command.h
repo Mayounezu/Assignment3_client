c446a07800f31e902a7958b4786a62327c7fd6e9
Frogy <thefrogeitan@gmail.com>
2025-01-27 15:02:19 +0200

    add commnd Class and Header to the project to take care of the command line arguments should had frame creation and send through the class.

---
+++ include/Command.h
@@ -0,0 +1,51 @@
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <map>
+
+class Command{
+
+private:
+    string std::commandName;
+    std::vector<string> commandArgs;
+
+public:
+    Command(string commandName, std::vector<string> commandArgs);
+    std::map<std::string, std::string> processCommand(){
+
+
+     }
+
+     void handleLoginCommand(){
+        
+     }
+
+     void handleJoinCommand(){
+        
+     }
+
+     void handleExitCommand(){
+        
+     }
+
+     void handleReportCommand(){
+        
+     }
+
+    void handleSummaryCommand(){
+        
+    }
+
+    void handleLogoutCommand(){
+        
+    }
+
+
+
+
+    
+
+
+
+}
